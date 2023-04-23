f6061317c67afa8cf0a509c16b3ae6d92d8dc10e
mkhinvasara6 <mkhinvasara6@gatech.edu>
2023-04-23 16:00:01 -0400

    adding tests

---
+++ test_sample.py
@@ -0,0 +1,10 @@
+from app import app # Flask instance of the API
+import timeit
+    
+ 
+def test_index_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+def test_time_it(benchmark):
+    benchmark(test_index_route)
