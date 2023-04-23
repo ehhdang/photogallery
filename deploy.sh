546f9a534bfe5028fd5f4241f3fc7239ed94a6c8
Emma Dang <danghoangha1996@gmail.com>
2023-04-23 17:31:27 -0400

    modified deploy.sh to remove all docker volumns

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=acatnamedsummer/photogalleryflaskapp:latest
 docker pull $DOCKER_IMAGE
 docker rm -f $(docker ps -aq)
+docker volumn prune -f
 docker run --name mariadbtest -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -p 3306:3306  -d mariadb
 docker run -d -p 5000:5000 --env-file .env $DOCKER_IMAGE
