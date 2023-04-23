085124ecc7a76d222d53ffa1d084a570480602df
mkhinvasara6 <mkhinvasara6@gatech.edu>
2023-04-23 18:13:43 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ DOCKER_IMAGE=acatnamedsummer/photogalleryflaskapp:latest
 docker pull $DOCKER_IMAGE
 docker rm -f $(docker ps -aq)
 docker run --name mariadbtest -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -p 3306:3306  -d mariadb
-docker run -d -p 5000:5000 --env-file .env $DOCKER_IMAGE
+docker run --name photogallery -d -p 5000:5000 --env-file .env $DOCKER_IMAGE
+
