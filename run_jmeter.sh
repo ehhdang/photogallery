ce1f5fdc9a1dd218f66e63c5a6adaae0d1ed5795
Jack Kelly <jkelly80@gatech.edu>
2023-04-23 20:08:38 -0400

    stuff

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz
-sudo apt install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -y
 tar xf apache-jmeter-5.5.tgz
 export PATH=$PATH:apache-jmeter-5.5/bin
 jmeter -n -t Photo-Gallery-Test-Plan.jmx -JPhotogalleryAddress=localhost -JOutputPath=./data.csv
