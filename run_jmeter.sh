faae0237c4e8e97b8b9cf62b1d4203d0881971d5
Jack Kelly <jkelly80@gatech.edu>
2023-04-23 23:54:51 -0400

    One more round of cleanup

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf apache-jmeter-5.5.tgz apache-jmeter-5.5 ./benchmarks/jmeter
+rm -rf apache-jmeter-5.5.tgz apache-jmeter-5.5 ./benchmarks/jmeter data.csv
 wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz
 sudo apt-get install openjdk-8-jdk -y
 tar xf apache-jmeter-5.5.tgz
